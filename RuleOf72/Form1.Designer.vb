6fd8de375b8cc32c0587767e61f009c5b8dcdda2
nguyhai <tenth11@gmail.com>
2018-09-18 02:01:39 -0500

    Add project files.

---
+++ RuleOf72/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmRuleOf72
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnEvaluate = New System.Windows.Forms.Button()
+        Me.lstOutput = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'btnEvaluate
+        '
+        Me.btnEvaluate.Location = New System.Drawing.Point(21, 12)
+        Me.btnEvaluate.Name = "btnEvaluate"
+        Me.btnEvaluate.Size = New System.Drawing.Size(283, 43)
+        Me.btnEvaluate.TabIndex = 0
+        Me.btnEvaluate.Text = "Evaluate Rule"
+        Me.btnEvaluate.UseVisualStyleBackColor = True
+        '
+        'lstOutput
+        '
+        Me.lstOutput.FormattingEnabled = True
+        Me.lstOutput.Location = New System.Drawing.Point(21, 86)
+        Me.lstOutput.Name = "lstOutput"
+        Me.lstOutput.Size = New System.Drawing.Size(283, 277)
+        Me.lstOutput.TabIndex = 1
+        '
+        'frmRuleOf72
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(324, 383)
+        Me.Controls.Add(Me.lstOutput)
+        Me.Controls.Add(Me.btnEvaluate)
+        Me.Name = "frmRuleOf72"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Rule 72"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnEvaluate As Button
+    Friend WithEvents lstOutput As ListBox
+End Class
---
+++ RuleOf72/Form1.vb
@@ -0,0 +1,37 @@
+﻿' The rule is used to approximate the time required for prices to double due to inflation
+' If the inflation rate is r%, then the rule of 72 estimates that prices will double in 72/r years. 
+' With an inflation rate of 6%, prices double in about 72/6 or 12 years
+' Write a program that will test the accuracy of this from 1 to 20% interest, and print it out to a list box
+
+Public Class frmRuleOf72
+
+    Private Sub btnEvaluate_Click(sender As Object, e As EventArgs) Handles btnEvaluate.Click
+        'Dim r As Double '  This is the interest rate
+        'Dim rule As Integer
+
+        'For r = 1 To 20
+        '    rule = Int(72 / r)
+        '    lstOutput.Items.Add(r & "%  " & rule & " years for prices to double")
+        'Next r
+
+        Dim years As Integer
+        Dim amount As Double
+
+        ' For every year btwn 1 and 20
+        For intRate As Integer = 1 To 20
+            years = 0
+            amount = 100
+
+            ' We will calculate within our do while loop, what is the rate for each year
+            While amount < 200 '  While the amount is less than 200, keep increasing it. Once it hits over 200, stop. This is to see how many years it takes to double
+                years += 1
+                amount = (1 + intRate / 100) * amount '  With each iteration, the percent goes up, the amount increases thanks to inflation
+            End While
+
+            lstOutput.Items.Add("Interest Rate " & (intRate / 100).ToString("P0"))
+            lstOutput.Items.Add("Rule of 72: " & Math.Round(72 / intRate))
+            lstOutput.Items.Add("Actual Doubling Time: " & years)
+            lstOutput.Items.Add("")
+        Next
+    End Sub
+End Class
